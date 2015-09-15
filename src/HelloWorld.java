407a7d85918e6c21ee114d09bceea2e71c0f0ad5
Jesse <Jesse@JesseQ.attlocal.net>
2015-09-15 00:07:05 -0500

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * This is my first Eclipse project...one thats says hello.
+ * @author Angelica Barajas
+ * @version 1.0 
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world!");  	
+	}
+
+}
